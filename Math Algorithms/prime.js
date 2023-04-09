1b34e0c65165603f811578f4389ab358d02397bd
rinshadcp <rinshadcp7337@gmail.com>
2023-04-09 14:22:28 +0530

    prime  number

---
+++ Math Algorithms/prime.js
@@ -0,0 +1,16 @@
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isPrime(5));
+console.log(isPrime(1));
+console.log(isPrime(100));
+
+// Big-O = O(n)

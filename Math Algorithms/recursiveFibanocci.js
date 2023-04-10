350d204a3863fa7c85f63b5ad9f77c2cd305bbd0
rinshadcp <rinshadcp7337@gmail.com>
2023-04-10 09:50:33 +0530

    recursive

---
+++ Math Algorithms/recursiveFibanocci.js
@@ -0,0 +1,12 @@
+function recursiveFibanocci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibanocci(n - 1) + recursiveFibanocci(n - 2);
+}
+console.log(recursiveFibanocci(0));
+console.log(recursiveFibanocci(1));
+console.log(recursiveFibanocci(7));
+
+
+// Big-O = O(2^n)

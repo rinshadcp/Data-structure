87c60281cde68cd7b7261b5f3c1fcb755eef5acf
rinshadcp <rinshadcp7337@gmail.com>
2023-04-11 09:42:29 +0530

    Recursive Binary Search

--- Search Algorithms/recursiveBinarySearch.js
+++ Search Algorithms/recursiveBinarySearch.js
@@ -1,21 +1,22 @@
-function recursiveBinarySearch(arr,target){
-    return search(arr,target,0,arr.length-1)
-function search(arr,target,leftIndex,rightIndex){
-    if(leftIndex>rightIndex){
-        return -1;
+function recursiveBinarySearch(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
+  function search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+      return -1;
     }
-    let middleIndex= Math.floor((leftIndex+rightIndex)/2);
-    if(target===arr[middleIndex]){
-        return middleIndex;
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
     }
-    if(target<arr[middleIndex]){
-        return search(arr,target,leftIndex,middleIndex-1)
-    }else{
-        return search(arr,target,middleIndex+1,rightIndex)
+    if (target < arr[middleIndex]) {
+      return search(arr, target, leftIndex, middleIndex - 1);
+    } else {
+      return search(arr, target, middleIndex + 1, rightIndex);
     }
+  }
 }
-    
-}
-console.log(recursiveBinarySearch([-1,3,6,8,9,10,25,55,100],100))
-console.log(recursiveBinarySearch([-1,3,6,8,9,10,25,55,100],8))
-console.log(recursiveBinarySearch([-1,3,6,8,9,10,25,55,100],300))
+console.log(recursiveBinarySearch([-1, 3, 6, 8, 9, 10, 25, 55, 100], 100));
+console.log(recursiveBinarySearch([-1, 3, 6, 8, 9, 10, 25, 55, 100], 8));
+console.log(recursiveBinarySearch([-1, 3, 6, 8, 9, 10, 25, 55, 100], 300));
+
+// Big-O = O(logn)

03d3325d71558a144301bb50278c953c08e6df04
rinshadcp <rinshadcp7337@gmail.com>
2023-04-18 10:06:56 +0530

    character Count using for ..of & regex

--- Practices/characterCount.js
+++ Practices/characterCount.js
@@ -1,15 +1,33 @@
+// function charCount(str) {
+//   let result = {};
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase();
+//     if (/[a-z0-9]/.test(char)) {
+//       if (result[char] > 0) {
+//         result[char]++;
+//       } else {
+//         result[char] = 1;
+//       }
+//     }
+//   }
+//   return result;
+// }
+// console.log(charCount("hallo Everyone!"));
+
+// By refractoring the code using (for ..of )
+
 function charCount(str) {
-  let result = {};
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i].toLowerCase();
+  let obj = {};
+  for (let chars of str) {
+    let char = chars.toLowerCase();
     if (/[a-z0-9]/.test(char)) {
-      if (result[char] > 0) {
-        result[char]++;
+      if (obj[char] > 0) {
+        obj[char]++;
       } else {
-        result[char] = 1;
+        obj[char] = 1;
       }
     }
   }
-  return result;
+  return obj;
 }
-console.log(charCount("hallo Everyone!"));
+console.log(charCount("Good Morning!!!"));

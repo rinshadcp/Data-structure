962e21ea5e0d449a53f5ab7b29b049f4147f2a6f
rinshadcp <rinshadcp7337@gmail.com>
2023-04-18 09:47:43 +0530

    character Count

---
+++ Practices/characterCount.js
@@ -0,0 +1,13 @@
+function charCount(str) {
+  let result = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (result[char] > 0) {
+      result[char]++;
+    } else {
+      result[char] = 1;
+    }
+  }
+  return result;
+}
+console.log(charCount("hallo Everyone!"));

f9f427c3222d045ed69b82c9685e5568d597218f
rinshadcp <rinshadcp7337@gmail.com>
2023-04-25 15:02:26 +0530

    insertion sort simple

--- Sorting Algorithms/insertionSort.js
+++ Sorting Algorithms/insertionSort.js
@@ -1,19 +1,45 @@
-const insertionSort = (array) => {
-  for (let i = 1; i < array.length; i++) {
-    let j = i - 1;
-    let current = array[i];
-    while (array[j] > current) {
-      array[j + 1] = array[j];
-      j--;
-    }
-    array[j + 1] = current;
-  }
-  return array;
-};
 
-let arr = [18, 48, 58, 9, 4, 67, 30];
-const result = insertionSort(arr);
+function insertionSort(arr) {
+    let i,j
+    for ( i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        for (j = i-1 ; arr[j] > currentVal; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentVal;
+    }
+    return arr;
+}
+
+const arr =[9,-7,6,5,4,4,43]
+const result = insertionSort(arr)
 console.log(result);
-result.forEach((element) => {
-  console.log(element);
-});
+
+
+
+
+
+
+
+
+
+
+// const insertionSort = (array) => {
+//   for (let i = 1; i < array.length; i++) {
+//     let j = i - 1;
+//     let current = array[i];
+//     while (array[j] > current) {
+//       array[j + 1] = array[j];
+//       j--;
+//     }
+//     array[j + 1] = current;
+//   }
+//   return array;
+// };
+
+// let arr = [18, 48, 58, 9, 4, 67, 30];
+// const result = insertionSort(arr);
+// console.log(result);
+// result.forEach((element) => {
+//   console.log(element);
+// });

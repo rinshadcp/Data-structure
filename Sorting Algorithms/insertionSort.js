255ee5cbe45bf3b9fda3a6f239b96ec1696c2ac6
rinshadcp <rinshadcp7337@gmail.com>
2023-04-25 14:48:26 +0530

    insertion sort

---
+++ Sorting Algorithms/insertionSort.js
@@ -0,0 +1,19 @@
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    let current = array[i];
+    while (array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array;
+};
+
+let arr = [18, 48, 58, 9, 4, 67, 30];
+const result = insertionSort(arr);
+console.log(result);
+result.forEach((element) => {
+  console.log(element);
+});

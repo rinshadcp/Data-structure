604025d4d7edb2d6322cd783b3f87e4de5721ff8
rinshadcp <rinshadcp7337@gmail.com>
2023-04-30 20:19:46 +0530

    for of used in linear search

--- Search Algorithms/linearSearch.js
+++ Search Algorithms/linearSearch.js
@@ -12,3 +12,14 @@ console.log(linearSearch([9, 3, 34, 45, 434, -2, 786], -2));
 console.log(linearSearch([9, 3, 34, 45, 434, -2, 786], 45));
 
 // Big-O = O(n)
+
+
+//example of for ..of
+
+function linearSearchs(array,value){
+  for(const[index,val] of array.entries()){
+    if(value===val)return index
+  }
+  return -1
+}
+console.log(linearSearchs([21,56,3,45,46,2,0,78],100))

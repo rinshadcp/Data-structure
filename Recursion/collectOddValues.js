d10be4ef7c24cc4e5ef41efa2a31b7e3a7b707b0
rinshadcp <rinshadcp7337@gmail.com>
2023-04-30 17:55:05 +0530

    collect odd values using pure method in recursion

--- Recursion/collectOddValues.js
+++ Recursion/collectOddValues.js
@@ -13,3 +13,21 @@ function collectOddValues(array){
     return result
 }
 console.log(collectOddValues([565,7,45,2,5,55,6,32,78,12,2,3]))
+
+
+
+//Pure method
+
+
+function collectOddValues(array){
+    let newArray=[]
+    if(array.length===0){
+        return newArray
+    }
+    if(array[0]%2 !==0){
+        newArray.push(array[0])
+    }
+    newArray=newArray.concat(collectOddValues(array.slice(1)))
+    return newArray
+}
+console.log(collectOddValues([3,5,6,1,2,8,9,7,8,1,10]))

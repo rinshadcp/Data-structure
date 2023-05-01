fbdd53fe0fc1e6ddbe6f24d7b97bea22e19ed431
rinshadcp <rinshadcp7337@gmail.com>
2023-05-01 13:14:43 +0530

    bubbleSort

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -1,51 +1,18 @@
-function bubbleSort(array){
-    let n= array.length
-    let i,j,temp
-    for(i=0 ; i< n ; i++){
-      for(j=0 ; j< n ; j++){
-        if( array[j] > array[j+1] ){
-          temp= array[j]
-          array[j]= array[j+1]
-          array[j+1]=temp
-        }
+function bubbleSort(arr){
+  for(let i=arr.length;i>0;i--){
+    for(let j=0;j<i-1;j++){
+      if(arr[j]>arr[j+1]){
+        let temp=arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1]=temp
       }
     }
-    return array
   }
-  
-  const array= [5,5,23,32,3,2,1,3,5,6]
-  console.log(bubbleSort(array))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  function bubbleSort(array){
-    let n=array.length;
-let temp;
-    for(let i=0;i<n;i++){
-    let currentValue= array[i]    
-    for(let j=0;j<n-i-1;j++){
-        if(array[j]>currentValue){
-            let temp = array[j]
-            array[j]= currentValue
-            currentValue=temp
-    }
-    }
+  return arr
 }
-  }
-  bubbleSort([2,5,7,6,9])
+console.log(bubbleSort([5,8,1,6,9,-1]))
+
+
+
+
+

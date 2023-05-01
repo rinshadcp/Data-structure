3328207d6f638aac5294ede92b2caf529296c507
rinshadcp <rinshadcp7337@gmail.com>
2023-05-01 16:37:33 +0530

    Insertion sort

--- Sorting Algorithms/insertionSort.js
+++ Sorting Algorithms/insertionSort.js
@@ -1,45 +1,13 @@
-
-function insertionSort(arr) {
-    let i,j
-    for ( i = 1; i < arr.length; i++) {
-        let currentVal = arr[i];
-        for (j = i-1 ; arr[j] > currentVal; j--) {
-            arr[j + 1] = arr[j];
+function insertionSort(arr){
+    for(let i=1;i<arr.length;i++){
+        let currentValue=arr[i]
+        for(var j=i-1;j>=0&&arr[j]>currentValue;j--){
+            arr[j+1]=arr[j]
         }
-        arr[j + 1] = currentVal;
+        arr[j+1]=currentValue
     }
     return arr;
 }
-
-const arr =[9,-7,6,5,4,4,43]
-const result = insertionSort(arr)
-console.log(result);
+console.log(insertionSort([8,6,2,4,-9,5,31,55,10]))
 
 
-
-
-
-
-
-
-
-
-// const insertionSort = (array) => {
-//   for (let i = 1; i < array.length; i++) {
-//     let j = i - 1;
-//     let current = array[i];
-//     while (array[j] > current) {
-//       array[j + 1] = array[j];
-//       j--;
-//     }
-//     array[j + 1] = current;
-//   }
-//   return array;
-// };
-
-// let arr = [18, 48, 58, 9, 4, 67, 30];
-// const result = insertionSort(arr);
-// console.log(result);
-// result.forEach((element) => {
-//   console.log(element);
-// });

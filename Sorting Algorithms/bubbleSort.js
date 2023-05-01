d86f9ee4af9b654c7b122252a0000a38789848d0
rinshadcp <rinshadcp7337@gmail.com>
2023-05-01 15:14:09 +0530

    bubbleSort using optimized method

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -31,5 +31,25 @@ console.log(bubbleSort1([5,8,1,6,9,-1]))
 
 
 
+//Optimized with noSwap
+function bubbleSort2(arr){
+  let noSwap;
+  for(let i=arr.length;i>0;i--){
+    noSwap=true;
+    for(let j=0;j<i-1;j++){
+      if(arr[j]>arr[j+1]){
+        let temp=arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1]=temp
+        noSwap=false
+      }
+    }
+    if(noSwap)break;
+  }
+  return arr
+}
+console.log(bubbleSort2([8,7,9,6,45,5,2,3,1,8,10]))
+
+
 
 

be88f1a8152edfbf18e4b3e31aa18417a3ba1381
rinshadcp <rinshadcp7337@gmail.com>
2023-05-01 15:44:01 +0530

    Selection Sort

---
+++ Sorting Algorithms/selectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr){
+for(let i=0;i<arr.length;i++){
+    let lowest = i
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[j]<arr[lowest]){
+            lowest=j
+        }
+   
+   
+    }
+    if(i!==lowest){
+        let temp=arr[i]
+        arr[i]=arr[lowest]
+        arr[lowest]=temp
+}
+}
+return arr;
+}
+console.log(selectionSort([5,8,1,6,9,7,10]))

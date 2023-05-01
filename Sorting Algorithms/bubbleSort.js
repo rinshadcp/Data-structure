742240d7e93af1375ff9c6dbc6da97fde9738d63
rinshadcp <rinshadcp7337@gmail.com>
2023-05-01 13:17:23 +0530

    bubbleSort using ES6

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -13,6 +13,23 @@ function bubbleSort(arr){
 console.log(bubbleSort([5,8,1,6,9,-1]))
 
 
+//ES6
+function bubbleSort1(arr){
+const swap=(arr,index1,index2)=>[arr[index1],arr[index2]]=[arr[index2],arr[index1]]
+  for(let i=arr.length;i>0;i--){
+    for(let j=0;j<i-1;j++){
+      if(arr[j]>arr[j+1]){
+       swap(arr,j,j+1)
+      }
+    }
+  }
+  return arr
+}
+console.log(bubbleSort1([5,8,1,6,9,-1]))
+
+
+
+
 
 
 

6679a476601f66e3321dc1f66a245bddd80d3e32
rinshadcp <rinshadcp7337@gmail.com>
2023-05-02 08:51:34 +0530

    Singly Linkedlist

---
+++ Linked List/singlyLinkedList.js
@@ -0,0 +1,45 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head=null
+        this.tail=null
+        this.length=0
+    }
+    push(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head=newNode
+            this.tail=this.head
+        }else{
+            this.tail.next=newNode
+            this.tail=newNode
+        }
+        this.length++
+        return this
+    }
+
+    pop(){
+        if(!this.head) return undefined
+        let current=this.head
+        let newTail=current
+        while(current.next){
+            newTail=current
+            current=current.next
+        }
+        this.tail=newTail
+        this.length--
+        if(this.length===0){
+            this.head=null
+            this.tail=null
+            return current
+        }
+
+    }
+}
+let list =new SinglyLinkedList()

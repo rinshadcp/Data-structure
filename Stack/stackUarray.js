4ae2d9a95da8fdfecec26abb2ae76383116be57d
rinshadcp <rinshadcp7337@gmail.com>
2023-05-04 19:55:19 +0530

    Stack implement using array

---
+++ Stack/stackUarray.js
@@ -0,0 +1,41 @@
+class Stack{
+    constructor(){
+        this.items=[]
+    }
+    push(item){
+        this.items.push(item)
+
+    }
+    pop(){
+        if(this.items.length===0){
+            return null
+        }
+         return this.items.pop()
+    }
+    peek(){
+        if(this.items.length===0){
+            return null
+        }
+        return this.items[this.items.length-1]
+    }
+    isEmpty(){
+        return this.items.length===0
+    }
+    size(){
+        return this.items.length
+    }
+    print(){
+        console.log(this.items)
+    }
+}
+
+const stack =new Stack()
+stack.push(3)
+stack.push(8)
+stack.push(5)
+stack.push(1)
+stack.push(7)
+stack.push(10)
+console.log( stack.peek())
+stack.pop()
+stack.print()

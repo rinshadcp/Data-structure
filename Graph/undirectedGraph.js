82ec047cdd9c45391de5e26956d94116e0ae66a8
rinshadcp <rinshadcp7337@gmail.com>
2023-05-11 14:16:34 +0530

    Graph represented using adjacencyList

---
+++ Graph/undirectedGraph.js
@@ -0,0 +1,30 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      let adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex.adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+let graph = new Graph();
+graph.addVertex("Tokyo");
+graph.addVertex("USA");
+graph.addVertex("Canada");
+graph.addEdge("Canada", "USA");
+console.log(graph);
